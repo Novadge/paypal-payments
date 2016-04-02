bbcfe897a882470b892d96edeba52e6a72878607
Burt Beckwith <burt@burtbeckwith.com>
2016-04-02 02:37:13 -0400

    cleanup

--- PaypalPaymentsGrailsPlugin.groovy
+++ PaypalPaymentsGrailsPlugin.groovy
@@ -1,69 +1,13 @@
 class PaypalPaymentsGrailsPlugin {
-    // the plugin version
     def version = "0.1.0"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
-
-    // TODO Fill in these fields
-    def title = "Paypal Payments Plugin" // Headline display name of the plugin
+    def title = "Paypal Payments Plugin"
     def author = "Omasirichukwu Joseph Udeinya"
     def authorEmail = "omasiri@novadge.com"
-    def description = '''\
-Lets grails applications Accept and process payments with paypal api
-'''
-
-    // URL to the plugin's documentation
+    def description = 'Lets Grails applications accept and process payments with Paypal REST API'
     def documentation = "http://grails.org/plugin/paypal"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-    def organization = [ name: "Novadge", url: "http://www.novadge.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-    def issueManagement = [ system: "GITHUB", url: "http://github.com/Novadge/paypal-payments/issues" ]
-
-    // Online location of the plugin's browseable source code.
-    def scm = [ url: "http://github.com/Novadge/paypal-payments/" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+    def license = "APACHE"
+    def organization = [name: "Novadge", url: "http://www.novadge.com/"]
+    def issueManagement = [url: "http://github.com/Novadge/paypal-payments/issues"]
+    def scm = [url: "http://github.com/Novadge/paypal-payments/"]
 }
